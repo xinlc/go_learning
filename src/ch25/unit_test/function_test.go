e66aef20a0f4556224e974f1e8b304828c62952a
lichao <1160060619@qq.com>
2019-04-08 16:53:49 +0800

    unit test

---
+++ src/ch25/unit_test/function_test.go
@@ -0,0 +1,33 @@
+package testing
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Error 后面代码会继续执行
+// Fatal 后面代码不会执行
+// 命令行运行要加参数 go test -v -conver
+func TestSquare(t *testing.T) {
+	inputs := [...]int{1, 2, 3}
+	expected := [...]int{1, 4, 9}
+	for i := 0; i < len(inputs); i++ {
+		ret := square(inputs[i])
+		if ret != expected[i] {
+			t.Errorf("input is %d, the expected is %d, the actual %d",
+				inputs[i], expected[i], ret)
+		}
+	}
+}
+
+func TestErrorInCode(t *testing.T) {
+	fmt.Println("Start")
+	t.Error("Error")
+	fmt.Println("End")
+}
+
+func TestFailInCode(t *testing.T) {
+	fmt.Println("Start")
+	t.Fatal("Error")
+	fmt.Println("End")
+}
