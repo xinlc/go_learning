9ced6962059b2dac6ce8fdfc8ea1f3b58d5dfe00
lichao <1160060619@qq.com>
2019-04-09 14:23:34 +0800

    assert

--- src/ch25/unit_test/function_test.go
+++ src/ch25/unit_test/function_test.go
@@ -3,11 +3,14 @@ package testing
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // Error 后面代码会继续执行
 // Fatal 后面代码不会执行
 // 命令行运行要加参数 go test -v -conver
+// 安装单元测试工具：go get -u github.com/stretchr/testify/assert
 func TestSquare(t *testing.T) {
 	inputs := [...]int{1, 2, 3}
 	expected := [...]int{1, 4, 9}
@@ -30,4 +33,13 @@ func TestFailInCode(t *testing.T) {
 	fmt.Println("Start")
 	t.Fatal("Error")
 	fmt.Println("End")
+}
+
+func TestSquareWithAssert(t *testing.T) {
+	inputs := [...]int{1, 2, 3}
+	expected := [...]int{1, 4, 9}
+	for i := 0; i < len(inputs); i++ {
+		ret := square(inputs[i])
+		assert.Equal(t, expected[i], ret)
+	}
 }
